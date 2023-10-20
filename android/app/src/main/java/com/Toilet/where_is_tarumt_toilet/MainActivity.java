5dabcc71b346fbaa5152a1cb3a025fd9d75ace0c
Timereader <53362690+wongjushao@users.noreply.github.com>
2023-10-21 00:29:56 +0800

    Initial commit

---
+++ android/app/src/main/java/com/Toilet/where_is_tarumt_toilet/MainActivity.java
@@ -0,0 +1,6 @@
+package com.Toilet.where_is_tarumt_toilet;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: const Text('Hello World'),
+      centerTitle: true,
+    ),
+    body: const Center(
+      child: Text('Hello World'),
+    ),
+    ),
+
+
+));
